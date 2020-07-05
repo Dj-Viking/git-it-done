f50ab9c6539475a5550e53b74ad925c2a1718ee1
DJ Viking <anders.swedishviking@gmail.com>
2020-07-05 19:06:32 -0400

    added more functionality to the function getuserRepos() now accepts a string argument as the username of anybody on github

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,28 @@
-function getUserRepos(){
-    fetch("https://api.github.com/users/octocat/repos");
+
+
+function getUserRepos(user){
+    //this takes a little bit longer than the console log outside the function
+    //so the outside log will print first
+    //this is to demonstrate that you want your code outside of the fetch to not get
+    //blocked by the fetch
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiUrl).then(function(response){
+        console.log("inside the function, first promise", response);
+        //converting the response into JSON instead of the response object
+        //if not JSON data then use response.text().then(function(data){})
+        response.json().then(function(data){
+            //returns another promise
+            console.log("here is the data we want, second promise");
+            console.log(data);
+        })
+    });
+    //status 200 means SUCCESS!!
+    
+    //storing the fetch in a variable creates a promise which this function now returns the value of the Promise
+    //var response = fetch("https://api.github.com/users/octocat/repos");
+    //console.log("got a Promise object response from the github API!!")
+    //console.log(response);
 }
-getUserRepos();
+// getUserRepos();
+console.log("outside of the function");
